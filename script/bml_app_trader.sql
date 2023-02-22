384588471a988e09aa95c9014862b5e82202efce
brandylyn <brandylynlanders@gmail.com>
2023-02-21 21:15:33 -0600

    making progress and filtering

--- script/bml_app_trader.sql
+++ script/bml_app_trader.sql
@@ -11,7 +11,24 @@ ORDER BY rating DESC;
 --columns to keep price, ratings
 --app_store_apps - name, size_bytes, currency, price, review_count, rating, content_rating, primary_genre
 --play_store_apps - name, category, rating, review_count, size, install_count, type, price, content_rating, genres
-SELECT name, category, rating, review_count, size, install_count, type, price, content_rating, genres 
+SELECT name, CAST(price AS money) AS price, rating, genres, content_rating, review_count 
 FROM play_store_apps
 UNION ALL
-SELECT 
+SELECT name, CAST(price AS money)AS price, rating, primary_genre, content_rating, CAST(review_count AS integer)AS review_count
+FROM app_store_apps
+ORDER BY rating, asc
+--Original script above--now on to filtering
+SELECT genres, COUNT(*) AS genre_count
+FROM (
+  SELECT name, CAST(price AS money) AS price, 
+	rating, genres, content_rating, review_count 
+  FROM play_store_apps
+  UNION ALL
+  SELECT name, CAST(price AS money) AS price, 
+	rating, primary_genre AS genres, content_rating, 
+	CAST(review_count AS integer) AS review_count
+  FROM app_store_apps
+) combined_data
+GROUP BY genres
+ORDER BY genre_count DESC;
+--Genre Count--
