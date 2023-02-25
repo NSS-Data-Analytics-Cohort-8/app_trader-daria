a1da516b2cb6a225505f0e78b038777c39696dda
Kevin Greer <kmgreer405@hotmail.com>
2023-02-25 10:36:10 -0500

    Making progress

--- scripts/app_trader.sql
+++ scripts/app_trader.sql
@@ -23,18 +23,24 @@ LIMIT 10;
 
 
 SELECT a.name AS apps_in_both,
-ROUND((a.rating + p.rating)/2, 1) AS both_stores_avg,
-a.price,
-p.price,
-SUM(CAST(a.review_count AS int) + p.review_count) AS total_review_count,
-p.install_count
+	CASE WHEN ROUND((a.rating + p.rating)/2, 1) < 0.5 THEN 1
+	WHEN ROUND((a.rating + p.rating)/2, 1) < 1.0 THEN 2
+	WHEN ROUND((a.rating + p.rating)/2, 1) < 1.5 THEN 3
+	WHEN ROUND((a.rating + p.rating)/2, 1) < 2.0 THEN 4
+	WHEN ROUND((a.rating + p.rating)/2, 1) < 2.5 THEN 5
+	WHEN ROUND((a.rating + p.rating)/2, 1) < 3.0 THEN 6
+	WHEN ROUND((a.rating + p.rating)/2, 1) < 3.5 THEN 7
+	WHEN ROUND((a.rating + p.rating)/2, 1) < 4.0 THEN 8
+	WHEN ROUND((a.rating + p.rating)/2, 1) < 4.5 THEN 9
+	WHEN ROUND((a.rating + p.rating)/2, 1) < 5.0 THEN 10 END AS years_on_store,
+a.price AS app_store_price,
+p.price AS play_store_price
 FROM app_store_apps AS a
 INNER JOIN play_store_apps As p
 ON a.name = p.name
-GROUP BY a.name, both_stores_avg, a.price, p.price, p.install_count
-ORDER BY both_stores_avg DESC,
-a.price,
-total_review_count DESC;
+GROUP BY a.name, years_on_store, a.price, p.price
+ORDER BY years_on_store DESC,
+a.price;
 
 SELECT *
 FROM play_store_apps
