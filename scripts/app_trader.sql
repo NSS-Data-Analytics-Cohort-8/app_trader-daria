e43ac66251ef6b7f8b512446066e63805eb25cdf
Kevin Greer <kmgreer405@hotmail.com>
2023-02-25 11:28:49 -0500

    progress maybe

--- scripts/app_trader.sql
+++ scripts/app_trader.sql
@@ -23,24 +23,36 @@ LIMIT 10;
 
 
 SELECT a.name AS apps_in_both,
-	CASE WHEN ROUND((a.rating + p.rating)/2, 1) < 0.5 THEN 1
-	WHEN ROUND((a.rating + p.rating)/2, 1) < 1.0 THEN 2
-	WHEN ROUND((a.rating + p.rating)/2, 1) < 1.5 THEN 3
-	WHEN ROUND((a.rating + p.rating)/2, 1) < 2.0 THEN 4
-	WHEN ROUND((a.rating + p.rating)/2, 1) < 2.5 THEN 5
-	WHEN ROUND((a.rating + p.rating)/2, 1) < 3.0 THEN 6
-	WHEN ROUND((a.rating + p.rating)/2, 1) < 3.5 THEN 7
-	WHEN ROUND((a.rating + p.rating)/2, 1) < 4.0 THEN 8
-	WHEN ROUND((a.rating + p.rating)/2, 1) < 4.5 THEN 9
-	WHEN ROUND((a.rating + p.rating)/2, 1) < 5.0 THEN 10 END AS years_on_store,
-a.price AS app_store_price,
-p.price AS play_store_price
+ROUND((a.rating + p.rating)/2, 1) AS rating_avg,
+	CASE WHEN ROUND((a.rating + p.rating)/2, 1) < 0.5 THEN 1*(10000*12)
+	WHEN ROUND((a.rating + p.rating)/2, 1) < 1.0 THEN 2*(10000*12)
+	WHEN ROUND((a.rating + p.rating)/2, 1) < 1.5 THEN 3*(10000*12)
+	WHEN ROUND((a.rating + p.rating)/2, 1) < 2.0 THEN 4*(10000*12)
+	WHEN ROUND((a.rating + p.rating)/2, 1) < 2.5 THEN 5*(10000*12)
+	WHEN ROUND((a.rating + p.rating)/2, 1) < 3.0 THEN 6*(10000*12)
+	WHEN ROUND((a.rating + p.rating)/2, 1) < 3.5 THEN 7*(10000*12)
+	WHEN ROUND((a.rating + p.rating)/2, 1) < 4.0 THEN 8*(10000*12)
+	WHEN ROUND((a.rating + p.rating)/2, 1) < 4.5 THEN 9*(10000*12)
+	WHEN ROUND((a.rating + p.rating)/2, 1) < 5.0 THEN 10*(10000*12) END AS app_lifetime_profit,
+	CASE WHEN CAST(REPLACE(REPLACE(p.price, '$',''),'0','1')AS numeric) > a.price THEN CAST(REPLACE(REPLACE(p.price, '$',''),'0','1')AS numeric)*10000
+	ELSE a.price*10000 END AS purchase_price, 
+	CASE WHEN ROUND((a.rating + p.rating)/2, 1) < 0.5 THEN 1*1000
+	WHEN ROUND((a.rating + p.rating)/2, 1) < 1.0 THEN 2*1000
+	WHEN ROUND((a.rating + p.rating)/2, 1) < 1.5 THEN 3*1000
+	WHEN ROUND((a.rating + p.rating)/2, 1) < 2.0 THEN 4*1000
+	WHEN ROUND((a.rating + p.rating)/2, 1) < 2.5 THEN 5*1000
+	WHEN ROUND((a.rating + p.rating)/2, 1) < 3.0 THEN 6*1000
+	WHEN ROUND((a.rating + p.rating)/2, 1) < 3.5 THEN 7*1000
+	WHEN ROUND((a.rating + p.rating)/2, 1) < 4.0 THEN 8*1000
+	WHEN ROUND((a.rating + p.rating)/2, 1) < 4.5 THEN 9*1000
+	WHEN ROUND((a.rating + p.rating)/2, 1) < 5.0 THEN 10*1000 END AS investment_cost
 FROM app_store_apps AS a
 INNER JOIN play_store_apps As p
 ON a.name = p.name
-GROUP BY a.name, years_on_store, a.price, p.price
-ORDER BY years_on_store DESC,
-a.price;
+GROUP BY a.name, rating_avg, app_lifetime_profit, purchase_price, investment_cost
+ORDER BY rating_avg DESC,
+app_lifetime_profit DESC,
+purchase_price;
 
 SELECT *
 FROM play_store_apps
